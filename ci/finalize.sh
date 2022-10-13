df5557382afde86cb2e75840dfdee192e201d818
Niharbabu D <nihar.devathi@smarsh.com>
2022-10-13 11:59:24 +0530

    testing

--- ci/finalize.sh
+++ ci/finalize.sh
@@ -5,11 +5,11 @@ set -eux
 git config --global user.email "ci@localhost"
 git config --global user.name "CI Bot"
 
-bosh upload-blobs
+# bosh upload-blobs
 
-git add -A
-git status
-git commit -m "Adding blobs via concourse"
+# git add -A
+# git status
+# git commit -m "Adding blobs via concourse"
 
 bosh create-release --final
 
